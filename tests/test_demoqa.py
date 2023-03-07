bb099eb260bbdfe9f414b0096c3503da55c7a9df
Heorhii Lazarevich <webremake@gmail.com>
2023-03-08 01:00:48 +0300

    test_student_registration_form started

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,3 +1,12 @@
+from selene import browser, have
 
 
 def test_student_registration_form():
+    browser.open('/')
+
+    browser.element('[class=practice-form-wrapper] h5').should(have.exact_text('Student Registration Form'))
+
+    browser.execute_script('document.querySelector("#fixedban").remove()')
+    browser.element('footer').execute_script('element.remove()')
+    browser.element('.sidebar-content').execute_script('element.remove()')
+
