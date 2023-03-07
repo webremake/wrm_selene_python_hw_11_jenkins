3d06bd3facda674f6519d2f1811908cc9a4263f4
Heorhii Lazarevich <webremake@gmail.com>
2023-03-08 00:59:06 +0300

    conftest.py file and browser_control ашч fixture created

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,14 @@
 import pytest
+from selene import browser
 
-@pytest.fixture()
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_control():
+    browser.config.hold_browser_open = True
+    # browser.config.browser_name = 'firefox'
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.window_width = '1360'
+    browser.config.window_height = '768'
+    browser.config.timeout = 6.0
+    yield
+    ...
