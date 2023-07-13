4cde727bdc0685816951377703d6b7eb0fa26da1
Heorhii Lazarevich <webremake@gmail.com>
2023-07-14 02:21:37 +0300

    run browser in selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,29 +24,46 @@ def load_env():
     load_dotenv()
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_mode",
+        action="store",
+        choices=["local", "selenoid"],
+        default="local",
+        help="Specify the browser mode: 'local' for local browser or 'selenoid' for Selenoid",
+    )
+
+
+@pytest.fixture(scope="session")
+def browser_mode(request):
+    return request.config.getoption("--browser_mode")
+
+
 @pytest.fixture(scope='function', autouse=True)
-def browser_control():
+def browser_control(browser_mode):
+    browser.config.hold_browser_open = 'True'
     browser.config.base_url = 'https://github.com/'
     browser.config.window_width = '1240'
     browser.config.window_height = '768'
     browser.config.timeout = 6.0
 
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
+    if browser_mode == "selenoid":
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "100.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
         }
-    }
 
-    login = os.getenv('SELENOID_LOGIN')
-    password = os.getenv('SELENOID_PASSWORD')
+        login = os.getenv('SELENOID_LOGIN')
+        password = os.getenv('SELENOID_PASSWORD')
 
-    browser.config.driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        desired_capabilities=selenoid_capabilities
-    )
+        browser.config.driver = webdriver.Remote(
+            command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+            desired_capabilities=selenoid_capabilities
+        )
 
     yield
     browser.quit()
